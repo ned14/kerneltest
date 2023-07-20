b636cfcdef59abf382781411827cc9c5b4044966
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2023-07-20 09:59:00 +0100

    Update to latest quickcpplib bootstrap script

--- cmake/QuickCppLibBootstrap.cmake
+++ cmake/QuickCppLibBootstrap.cmake
@@ -29,15 +29,39 @@ foreach(item ${CMAKE_MODULE_PATH})
   endif()
 endforeach()
 if(NOT quickcpplib_done)
+  find_package(quickcpplib QUIET CONFIG)
+  if(quickcpplib_FOUND)
+    if(DEFINED quickcpplib_CMAKELIB_DIR AND DEFINED quickcpplib_SCRIPTS_DIR)
+      set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${quickcpplib_CMAKELIB_DIR}")
+      set(CTEST_QUICKCPPLIB_SCRIPTS "${quickcpplib_SCRIPTS_DIR}")
+      set(quickcpplib_done ON)
+    elseif(EXISTS "${quickcpplib_DIR}/share/cmakelib")
+      set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${quickcpplib_DIR}/share/cmakelib")
+      set(CTEST_QUICKCPPLIB_SCRIPTS "${quickcpplib_DIR}/share/scripts")
+      set(quickcpplib_done ON)
+    elseif(EXISTS "${quickcpplib_DIR}/../../../share/cmakelib")
+      set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${quickcpplib_DIR}/../../../share/cmakelib")
+      set(CTEST_QUICKCPPLIB_SCRIPTS "${quickcpplib_DIR}/../../../share/scripts")
+      set(quickcpplib_done ON)
+    endif()
+  endif()
+endif()
+#message("*** ${CMAKE_MODULE_PATH}")
+if(NOT quickcpplib_done)
   # CMAKE_SOURCE_DIR is the very topmost parent cmake project
   # CMAKE_CURRENT_SOURCE_DIR is the current cmake subproject
   if(NOT DEFINED CTEST_QUICKCPPLIB_CLONE_DIR)
-    # If there is a magic .quickcpplib_use_siblings directory above the topmost project, use sibling edition
     if(EXISTS "${CMAKE_SOURCE_DIR}/../.quickcpplib_use_siblings" AND NOT QUICKCPPLIB_DISABLE_SIBLINGS)
+      # If there is a magic .quickcpplib_use_siblings directory above the topmost project, use sibling edition
       set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_SOURCE_DIR}/../quickcpplib/cmakelib")
       set(CTEST_QUICKCPPLIB_SCRIPTS "${CMAKE_SOURCE_DIR}/../quickcpplib/scripts")
       # Copy latest version of myself into end user
       file(COPY "${CTEST_QUICKCPPLIB_SCRIPTS}/../cmake/QuickCppLibBootstrap.cmake" DESTINATION "${CMAKE_SOURCE_DIR}/cmake/")
+    elseif(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/../.quickcpplib_use_siblings" AND NOT QUICKCPPLIB_DISABLE_SIBLINGS)
+      set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_CURRENT_SOURCE_DIR}/../quickcpplib/cmakelib")
+      set(CTEST_QUICKCPPLIB_SCRIPTS "${CMAKE_CURRENT_SOURCE_DIR}/../quickcpplib/scripts")
+    elseif(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/quickcpplib/repo/cmakelib")
+      set(CTEST_QUICKCPPLIB_CLONE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/quickcpplib")
     elseif(CMAKE_BINARY_DIR)
       # Place into root binary directory, same place as where find_quickcpplib_library() puts dependencies.
       set(CTEST_QUICKCPPLIB_CLONE_DIR "${CMAKE_BINARY_DIR}/quickcpplib")
@@ -50,14 +74,9 @@ if(NOT quickcpplib_done)
   if(CTEST_QUICKCPPLIB_CLONE_DIR)
     if(NOT EXISTS "${CTEST_QUICKCPPLIB_CLONE_DIR}/repo/cmakelib")
       file(MAKE_DIRECTORY "${CTEST_QUICKCPPLIB_CLONE_DIR}")
-      find_package(quickcpplib QUIET CONFIG PATHS "${CTEST_QUICKCPPLIB_CLONE_DIR}/repo")
-      if(quickcpplib_FOUND)
-      endif()
-    endif()
-    if(NOT EXISTS "${CTEST_QUICKCPPLIB_CLONE_DIR}/repo/cmakelib")
       message(STATUS "quickcpplib not found, cloning git repository and installing into ${CTEST_QUICKCPPLIB_CLONE_DIR} ...")
       include(FindGit)
-      execute_process(COMMAND "${GIT_EXECUTABLE}" clone "https://github.com/ned14/quickcpplib.git" repo
+      execute_process(COMMAND "${GIT_EXECUTABLE}" clone --recurse-submodules --depth 1 --jobs 8 --shallow-submodules "https://github.com/ned14/quickcpplib.git" repo
         WORKING_DIRECTORY "${CTEST_QUICKCPPLIB_CLONE_DIR}"
         OUTPUT_VARIABLE cloneout
         ERROR_VARIABLE errout
